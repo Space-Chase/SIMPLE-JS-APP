b4346184de036a830453c8799341f52ecea3d6fa
Space-Chase <chasedraizin@gmail.com>
2023-01-25 21:26:14 -0500

    for each added over generic loop and IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,35 @@
-let pokemonList = [
-    { name: "BULBASAUR", weight: 6.9, height: 0.7, type: ['GRASS', 'POISON'] },
-    { name: "IVYSAUR", weight: 10, height: 1.0, type: ['GRASS', 'POISON'] },
-    { name: "VENUSAUR", weight: 100.0, height: 2.0, type: ['GRASS', 'POISON'] },
-    { name: "CHARMANDER", weight: 8.5, height: 0.6, trpe: ['FIRE'] },
-    { name: "CHARMELEON", weight: 19.0, height: 1.1, type: ['FIRE'] },
-    { name: "CHARIZARD", weight: 90.5, height: 1.7, type: ['FIRE', 'FLYING'] },
-    { name: "SQUIRTLE", weight: 9.0, height: 0.5, type: ['WATER'] },
-    { name: "WARTURTLE", weight: 22.5, height: 1.0, type: ['WATER'] },
-    { name: "BLASTOISE", weight: 85.5, height: 1.6, type: ['WATER'] }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: "BULBASAUR", weight: 6.9, height: 0.7, type: ["GRASS", "POISON"] },
+        { name: "IVYSAUR", weight: 10, height: 1.0, type: ["GRASS", "POISON"] },
+        { name: "VENUSAUR", weight: 100.0, height: 2.0, type: ["GRASS", "POISON"] },
+        { name: "CHARMANDER", weight: 8.5, height: 0.6, type: ["FIRE"] },
+        { name: "CHARMELEON", weight: 19.0, height: 1.1, type: ["FIRE"] },
+        { name: "CHARIZARD", weight: 90.5, height: 1.7, type: ["FIRE", "FLYING"] },
+        { name: "SQUIRTLE", weight: 9.0, height: 0.5, type: ["WATER"] },
+        { name: "WARTURTLE", weight: 22.5, height: 1.0, type: ["WATER"] },
+        { name: "BLASTOISE", weight: 85.5, height: 1.6, type: ["WATER"] }
+    ];
 
-for (let i = 0; i < pokemonList.length; i++) {
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     let tallP = ""
-    if (pokemonList[i].height > 1.9) {
+    if (pokemon.height > 1.9) {
         tallP = '  -Wow thats tall!'
     }
-    document.write(pokemonList[i].name + ' (height:) ' + pokemonList[i].height + (tallP) + "<br>");
-}
+    document.write(pokemon.name + ' (height:) ' + pokemon.height + (tallP) + "<br>");
+
+});
